b0731f45e690da8e64ab44f9482ea554ff77e5e4
JUBILEU404 <locaytonfenix@gmail.com>
2024-03-23 16:22:38 -0300

    atualizações

--- App.jsx
+++ App.jsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import Person from './Person';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Person nome="Sven" idade="19"/>
+      <img src="./iphone15.jpg" alt="" width={250} />
+      <Person preco="R$9.699,00" marca="Apple" nomeMD="iPhone 15 Pro Max" system="iOS 16" tecno="5G" ficha="Conteúdo da caixa
+
+iPhone com iOS 17, Cabo de carregamento USB-C (1m), Documentação
+
+Altura
+
+159,9 mm
+
+Largura
+
+76,7 mm
+
+Profundidade
+
+8,25 mm
+
+Peso
+
+221 gramas
+
+Anatel
+
+12782-23-01993" />
     </View>
   );
 }
@@ -16,5 +39,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 0,
   },
 });
--- Person.jsx
+++ Person.jsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text } from 'react-native';
 
-const Person = () => {
+const Person = (props) => {
+  const { preco, marca, nomeMD, system, tecno, ficha, } = props;
+
   return (
     <View>
-      <Text>Meu Nome e </Text> person.nome
+      <h2>Apple iPhone 15 Pro Max (512 GB) — Titânio Azul</h2>
+      <h4>Valor: {preco} </h4>
+      <Text>Marca: {marca}</Text>
+      <Text>Nome do modelo: {nomeMD}</Text>
+      <Text>Sistema operacional {system}</Text>
+      <Text>Tecnologia celular: {tecno}</Text>
+      <Text>ficha tecnica: {ficha}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Person
+export default Person;
